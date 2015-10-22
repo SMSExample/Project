caee771ad07f16be7846acee2830425c1d0ba92c
Oshyrath <GuyAvrMaor@gmail.com>
2015-10-22 17:35:29 -0500

    Merge branch 'master' of C:\Users\Guy\AndroidStudioProjects\SendAndReceiveExample\Project with conflicts.

---
+++ app/src/main/java/com/example/guy/smsclassproject/TextingActivity.java
@@ -0,0 +1,22 @@
+package com.example.guy.smsclassproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class TextingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_texting);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
