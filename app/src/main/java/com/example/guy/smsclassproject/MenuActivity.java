0a9f5f3f545b0557c1f5dead3b62f3cec2e1a5f5
txz141530 <IneedJesus@2015>
2015-10-27 17:55:58 -0500

    Created the Menu Activity. You can now press one of two buttons and it will take you to either Drafts or History.

--- app/src/main/java/com/example/guy/smsclassproject/MenuActivity.java
+++ app/src/main/java/com/example/guy/smsclassproject/MenuActivity.java
@@ -1,13 +1,43 @@
 package com.example.guy.smsclassproject;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends Activity {
 
+    private Button toDrafts;
+    private Button toHistory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        toDrafts = (Button) findViewById(R.id.goToDrafts);
+        toDrafts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                goToDrafts();
+            }
+        });
+        toHistory = (Button) findViewById(R.id.goToTextHistory);
+        toHistory.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                goToTextHistory();
+            }
+        });
+    }
+    //called when user touch the button, learned from http://developer.android.com/guide/topics/ui/controls/button.html
+    public void goToDrafts( ){
+        startActivity(new Intent(MenuActivity.this, DraftsActivity.class));
+    }
+
+    //called when user touch the button, learned from http://developer.android.com/guide/topics/ui/controls/button.html
+    public void goToTextHistory(){
+        startActivity(new Intent(MenuActivity.this, HistoryActivity.class));
     }
 }
--- app/src/main/java/com/example/guy/smsclassproject/TextingActivity.java
+++ app/src/main/java/com/example/guy/smsclassproject/TextingActivity.java
@@ -12,7 +12,7 @@ public class TextingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_texting);
+        setContentView(R.layout.activity_texting);
 
 
 
