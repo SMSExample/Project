e5141691b6a9c727b66552ca3543440ebecd9f5e
Oshyrath <GuyAvrMaor@gmail.com>
2015-11-05 18:17:42 -0600

    Update on DraftsActivityTest

--- app/src/test/java/com/example/guy/smsclassproject/DraftsActivityTest.java
+++ app/src/test/java/com/example/guy/smsclassproject/DraftsActivityTest.java
@@ -1,6 +1,7 @@
 package com.example.guy.smsclassproject;
 
 import android.content.DialogInterface;
+import android.provider.Telephony;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.*;
 import android.widget.*;
@@ -10,7 +11,7 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
+import android.content.*;
 import java.util.ArrayList;
 
 /**
@@ -35,7 +36,8 @@ public class DraftsActivityTest extends ActivityInstrumentationTestCase2<DraftsA
     public void setUp() throws Exception
     {
         super.setUp();
-        //DraftsActivity activityMonitor = getInstrumentation().addMonitor(NextActivity.class.getName(), null, false);
+       // DraftsActivity activityMonitor = getInstrumentation().addMonitor(NextActivity.class.getName(), null, false);
+
         draftsDatabase = new DraftsDatabase();
         messageObject1 = new MessageObject("hi","5554",true);
         messageObject2 = new MessageObject("hi hi","5555554",true);
@@ -44,7 +46,9 @@ public class DraftsActivityTest extends ActivityInstrumentationTestCase2<DraftsA
         draftsDatabase.addMessage(messageObject2);
         draftsDatabase.addMessage(messageObject3);
         messagesToBeDisplayed = draftsDatabase.getAllTexts();
-        tester = new DraftsActivity();                                  //getActivity();
+        Intent intent = new Intent(getInstrumentation().getTargetContext(), DraftsActivity.class);
+        setActivityIntent(intent);
+        tester = getActivity();
         messagesToBeDisplayed = tester.messagesToBeDisplayed;
         searchText = (EditText) tester.findViewById(R.id.searchText);
         searchButton = (Button) tester.findViewById(R.id.searchButton);
@@ -58,7 +62,7 @@ public class DraftsActivityTest extends ActivityInstrumentationTestCase2<DraftsA
         searchText.setText("hi");
         searchButton.performClick();
         messagesToBeDisplayed=draftsDatabase.getMessagesByKey(searchText.getText().toString());
-        assertEquals("Messages with the word hi",2,messagesToBeDisplayed.size());
+        assertEquals(100,messagesToBeDisplayed.size());
 
         searchText.setText("sup");
         searchButton.performClick();
