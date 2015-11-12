63b2d1e1a837a4c00245a5aa85e2df31c4fc6363
TianXZheng <txz141530@utdallas.edu>
2015-11-12 16:20:20 -0600

    added runtime permission request for RECEIVE_SMS and READ_CONTACTS in the MenuActivity

--- app/src/main/java/com/example/guy/smsclassproject/MenuActivity.java
+++ app/src/main/java/com/example/guy/smsclassproject/MenuActivity.java
@@ -3,7 +3,9 @@ package com.example.guy.smsclassproject;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
@@ -12,9 +14,29 @@ import java.util.jar.Manifest;
 
 public class MenuActivity extends Activity {
 
+    private static final int MY_PERMISSIONS_REQUEST_RECEIVE_SMS = 2;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 3;
     private Button toDrafts;
     private Button toHistory;
     private Button toTexting;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_RECEIVE_SMS:{
+                if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){;}
+                else{;}
+                return;
+            }
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:{
+                if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){;}
+                else{;}
+                return;
+            }
+        }
+    }
+
     @Override
     /**
      * Find all the Views of the MenuActivity.xml and give all the Buttons their
@@ -22,6 +44,18 @@ public class MenuActivity extends Activity {
      * @Param savedInstanceState The Bundle that is needed to call the super.onCreate().
      */
     protected void onCreate(Bundle savedInstanceState) {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECEIVE_SMS)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.RECEIVE_SMS)){;}
+            else{
+                ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.RECEIVE_SMS},MY_PERMISSIONS_REQUEST_RECEIVE_SMS);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)!=PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)){;}
+            else{
+                ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.READ_CONTACTS},MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         //The button is retrieved from the layout.
@@ -42,8 +76,7 @@ public class MenuActivity extends Activity {
         });
         toTexting = (Button) findViewById(R.id.textButton);
         toTexting.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
 
                 goToTexting();
             }
