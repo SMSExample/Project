c81706ba6682709fa43a3a09ba88171672361b06
TianXZheng <txz141530@utdallas.edu>
2015-11-12 15:54:50 -0600

    added runtime permission request for SEND_SMS

--- app/src/main/java/com/example/guy/smsclassproject/MenuActivity.java
+++ app/src/main/java/com/example/guy/smsclassproject/MenuActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.jar.Manifest;
+
 public class MenuActivity extends Activity {
 
     private Button toDrafts;
--- app/src/main/java/com/example/guy/smsclassproject/TextingActivity.java
+++ app/src/main/java/com/example/guy/smsclassproject/TextingActivity.java
@@ -1,17 +1,21 @@
 package com.example.guy.smsclassproject;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
@@ -23,7 +27,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class TextingActivity extends AppCompatActivity {
-
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
     //All values used on this page
     Button sendButton;
     Button saveButton;
@@ -77,11 +81,29 @@ public class TextingActivity extends AppCompatActivity {
         }
     };
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){;}
+                else {;}
+                return;
+            }
+        }
+    }
+
     /**
      * actions done when the activity starts
      * @param savedInstanceState android class
      */
     protected void onCreate(Bundle savedInstanceState) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.SEND_SMS)){;}
+            else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_texting);
         intentFilter = new IntentFilter();
