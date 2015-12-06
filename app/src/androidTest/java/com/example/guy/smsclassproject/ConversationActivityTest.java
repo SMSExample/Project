55a07d6e270ca7f096c341c1e54e31fe97df5e90
Kenneth Leon <ksl130230@utdallas.edu>
2015-12-05 23:49:56 -0600

    I finished the DraftsActivityTest and added the ConversationActivityTest. I can't run the tests so I'll need Guy to tell me if they work or not. Lemme know what else I can do for the tests.

---
+++ app/src/androidTest/java/com/example/guy/smsclassproject/ConversationActivityTest.java
@@ -0,0 +1,75 @@
+package com.example.guy.smsclassproject;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.junit.BeforeClass;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kenneth on 12/4/2015.
+ */
+public class ConversationActivityTest extends ActivityInstrumentationTestCase2 <ConversationActivity> {
+
+    private ConversationActivity tester;
+    private MessageDatabase testmessageDatabase;
+    private TextView pageNumber;
+    private Button prevButton;
+    private Button nextButton;
+    private MessageObject messageObject1;
+    private MessageObject messageObject2;
+    private MessageObject messageObject3;
+    Button[] correspondentButtons;
+
+    ArrayList<MessageObject> messagesToBeDisplayed;
+
+    public ConversationActivityTest() {
+        super(ConversationActivity.class);
+    }
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        //super.setUp();
+
+        testmessageDatabase = new MessageDatabase();
+        messageObject1 = new MessageObject("hi", "5554",null, true);
+        messageObject2 = new MessageObject("what's up", "5554",null, true);
+        messageObject3 = new MessageObject("sup", "5435555554",null, true);
+        testmessageDatabase.addMessage(messageObject1);
+        testmessageDatabase.addMessage(messageObject2);
+        testmessageDatabase.addMessage(messageObject3);
+        messagesToBeDisplayed = testmessageDatabase.getAllTexts();
+        tester = getActivity();
+        messagesToBeDisplayed = tester.messagesToBeDisplayed;
+        prevButton = (Button) tester.findViewById(R.id.prevButton);
+        nextButton = (Button) tester.findViewById(R.id.nextButton);
+    }
+
+    @SmallTest
+    @UiThreadTest
+    public void testShouldAddMessage() {
+
+        assertNotNull(tester.correspondentButtons[0]);
+        String buttonText0 = correspondentButtons[0].getText().toString();
+        if(buttonText0.equals("5554: hi"))
+            assertSame("Conversation with 5554 grouped successfully", buttonText0, messageObject1);
+
+        assertNotNull(tester.correspondentButtons[1]);
+        String buttonText1 = correspondentButtons[1].getText().toString();
+        if(buttonText1.equals("5435555554: sup"))
+            assertSame("Conversation with 5435555554 grouped successfully", buttonText1, messageObject3);
+    }
+}
--- app/src/androidTest/java/com/example/guy/smsclassproject/DraftsActivityTest.java
+++ app/src/androidTest/java/com/example/guy/smsclassproject/DraftsActivityTest.java
@@ -19,6 +19,8 @@ public class DraftsActivityTest extends ActivityInstrumentationTestCase2<DraftsA
     private MessageObject messageObject1;
     private MessageObject messageObject2;
     private MessageObject messageObject3;
+    Button[] draftButtons;
+
     ArrayList<MessageObject> messagesToBeDisplayed;
 
     public DraftsActivityTest() {
@@ -41,6 +43,7 @@ public class DraftsActivityTest extends ActivityInstrumentationTestCase2<DraftsA
         messagesToBeDisplayed = tester.messagesToBeDisplayed;
         searchText = (EditText) tester.findViewById(R.id.searchText);
         searchButton = (Button) tester.findViewById(R.id.searchButton);
+
 
     }
 
@@ -69,5 +72,46 @@ public class DraftsActivityTest extends ActivityInstrumentationTestCase2<DraftsA
         assertEquals("Messages with the word i", 2, messagesToBeDisplayed.size());
 
     }
+
+    @SmallTest
+    @UiThreadTest
+    public void testRedisplay()
+    {
+        assertNotNull(tester.draftButtons[0]);
+        draftButtons[0].performClick();
+        assertEquals("Size of the list after deletion is 2", 2, messagesToBeDisplayed.size()); //presses the first button, which deletes it from the drafts
+
+        String buttonText0 = draftButtons[0].getText().toString();
+        if(buttonText0.equals("5555554: hi hi"))
+            assertSame("Text redisplayed on the first button", buttonText0, messageObject2); //gets the text of the current button, since the messageobject1 was in draftsButtons0 before, not it should have messageobject2
+
+        assertNotNull(tester.draftButtons[1]);
+        String buttonText1 = draftButtons[1].getText().toString();
+        if(buttonText1.equals("5435555554: sup"))
+            assertSame("Text redisplayed on the second button", buttonText1, messageObject3);
+
+    }
+
+    @SmallTest
+    @UiThreadTest
+    public void testMessageButtons()
+    {
+
+        assertNotNull(tester.draftButtons[1]);
+        draftButtons[1].performClick();
+
+        assertNotNull(tester.draftButtons[2]);
+        draftButtons[2].performClick();
+
+        assertEquals("The draftsDatabase now only contains 1 message", 1, messagesToBeDisplayed.size());
+
+        assertNotNull(tester.draftButtons[0]);
+        draftButtons[0].performClick();
+
+        assertNull(draftsDatabase); //after you press all the buttons, the draftsDatabase should be empty because all the messages have been deleted
+
+    }
+
+
 
 }
